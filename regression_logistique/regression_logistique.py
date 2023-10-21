eb510dc410b34b2f89f015326126bfa45811e23b
André Meneses <andre_meneses2001@hotmail.com>
2023-10-21 11:07:12 -0400

    docs: Add references files

--- regression_logistique/regression_logistique.py
+++ regression_logistique/regression_logistique.py
@@ -6,8 +6,16 @@ class LogisticRegression:
     def __init__(self):
         # Class constructor
 
-    def _cross_entropy_loss(self):
-        # Log loss function    
+    def _cross_entropy_loss(self, y_prediction, y_labels):
+        loss = 0
+
+        for y,yh in zip(y_prediction, y_labels):
+            loss += -yh*np.log(y)
+
+        return loss/y_prediction.shape[0]
+
+    def _sigmoide(self):
+
 
     def _initialize_weights(self, variance):
         # Initially, gaussian distribution with mean = 0
